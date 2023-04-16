13c83d1b0de844f7a9164239b91b7cec3376d764
thedavidmeister <thedavidmeister@gmail.com>
2023-04-16 19:10:57 +0400

    fix slither

--- src/deprecated/IFlowERC721V1.sol
+++ src/deprecated/IFlowERC721V1.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: CAL
-pragma solidity =0.8.18;
+pragma solidity ^0.8.18;
 
 import "rain.interface.interpreter/deprecated/IInterpreterCallerV1.sol";
 import "rain.interface.interpreter/LibEvaluable.sol";
